18a37ae0a5c98483ba5cb2375219b4b3c93223e5
Liron Barel <Vallejo-233@greendreamteam.onmicrosoft.com>
2021-06-26 16:01:20 +0300

    add edit for managers

--- client/src/app/components/items/items.component.ts
+++ client/src/app/components/items/items.component.ts
@@ -21,9 +21,7 @@ import { CountryModel } from "../../models/countries.model";
 })
 export class ItemsComponent implements OnInit {
   items: ItemModel[];
-  countries: CountryModel[];
-  name: string;
-  price: string;
+  selectedItem: ItemModel;
 
   constructor(
     private itemService: ItemService,
@@ -36,7 +34,6 @@ export class ItemsComponent implements OnInit {
 
   ngOnInit() {
     this.fetchItems();
-    this.setCountries();
   }
 
   fetchItems() {
@@ -68,23 +65,25 @@ export class ItemsComponent implements OnInit {
 
   deleteItem(itemId: string) {
     // TODO: remove from screen and give the user a message of success
-    this.itemService.deleteItem(itemId).subscribe(res => {
-        console.log("res",res)
-    })
+    this.itemService.deleteItem(itemId).subscribe((res) => {
+      console.log("res", res);
+    });
   }
 
   updateItem(item: ItemModel) {
     // TODO: update from screen and give the user a message of success
-    this.itemService.updateItem(item).subscribe(res => {
-        console.log("res",res)
-    })
+    this.itemService.updateItem(item).subscribe((res) => {
+      console.log("res", res);
+    });
   }
 
-  onRegisterUserItem(form: NgForm) {
-    let userItem = {
-      name: this.name,
-      price: this.price,
-    };
+  setItemForEdit(item) {
+    this.selectedItem = item;
+  }
+
+  onEditItem(form: NgForm, event) {
+    this.selectedItem = { ...this.selectedItem, ...form.value };
+
     //Check for valid contact number
     /*
     if (!this.validateService.validateContactNumber(userItem.contact_info)) {
@@ -98,7 +97,8 @@ export class ItemsComponent implements OnInit {
       return false;
     }
     */
-    this.userItemService.saveUserItem(userItem).subscribe((data) => {
+
+    this.itemService.updateItem(this.selectedItem).subscribe((data) => {
       if (data) {
         this.flashMessage.showFlashMessage({
           messages: ["Item added to your cart!"],
@@ -106,274 +106,9 @@ export class ItemsComponent implements OnInit {
           timeout: 4000,
           type: "success",
         });
-        form.resetForm();
-        this.router.navigate(["items"]);
+        this.fetchItems();
         return true;
       }
     });
-  }
-
-  setCountries() {
-    this.countries = [
-      "Afghanistan",
-      "Aland Islands",
-      "Albania",
-      "Algeria",
-      "American Samoa",
-      "Andorra",
-      "Angola",
-      "Anguilla",
-      "Antarctica",
-      "Antigua",
-      "Argentina",
-      "Armenia",
-      "Aruba",
-      "Australia",
-      "Austria",
-      "Azerbaijan",
-      "Bahamas",
-      "Bahrain",
-      "Bangladesh",
-      "Barbados",
-      "Barbuda",
-      "Belarus",
-      "Belgium",
-      "Belize",
-      "Benin",
-      "Bermuda",
-      "Bhutan",
-      "Bolivia",
-      "Bosnia",
-      "Botswana",
-      "Bouvet Island",
-      "Brazil",
-      "British Indian Ocean Trty.",
-      "Brunei Darussalam",
-      "Bulgaria",
-      "Burkina Faso",
-      "Burundi",
-      "Caicos Islands",
-      "Cambodia",
-      "Cameroon",
-      "Canada",
-      "Cape Verde",
-      "Cayman Islands",
-      "Central African Republic",
-      "Chad",
-      "Chile",
-      "China",
-      "Christmas Island",
-      "Cocos (Keeling) Islands",
-      "Colombia",
-      "Comoros",
-      "Congo",
-      "Congo, Democratic Republic of the",
-      "Cook Islands",
-      "Costa Rica",
-      "Cote d'Ivoire",
-      "Croatia",
-      "Cuba",
-      "Cyprus",
-      "Czech Republic",
-      "Denmark",
-      "Djibouti",
-      "Dominica",
-      "Dominican Republic",
-      "Ecuador",
-      "Egypt",
-      "El Salvador",
-      "Equatorial Guinea",
-      "Eritrea",
-      "Estonia",
-      "Ethiopia",
-      "Falkland Islands (Malvinas)",
-      "Faroe Islands",
-      "Fiji",
-      "Finland",
-      "France",
-      "French Guiana",
-      "French Polynesia",
-      "French Southern Territories",
-      "Futuna Islands",
-      "Gabon",
-      "Gambia",
-      "Georgia",
-      "Germany",
-      "Ghana",
-      "Gibraltar",
-      "Greece",
-      "Greenland",
-      "Grenada",
-      "Guadeloupe",
-      "Guam",
-      "Guatemala",
-      "Guernsey",
-      "Guinea",
-      "Guinea-Bissau",
-      "Guyana",
-      "Haiti",
-      "Heard",
-      "Herzegovina",
-      "Holy See",
-      "Honduras",
-      "Hong Kong",
-      "Hungary",
-      "Iceland",
-      "India",
-      "Indonesia",
-      "Iran (Islamic Republic of)",
-      "Iraq",
-      "Ireland",
-      "Isle of Man",
-      "Israel",
-      "Italy",
-      "Jamaica",
-      "Jan Mayen Islands",
-      "Japan",
-      "Jersey",
-      "Jordan",
-      "Kazakhstan",
-      "Kenya",
-      "Kiribati",
-      "Korea",
-      "Korea (Democratic)",
-      "Kuwait",
-      "Kyrgyzstan",
-      "Lao",
-      "Latvia",
-      "Lebanon",
-      "Lesotho",
-      "Liberia",
-      "Libyan Arab Jamahiriya",
-      "Liechtenstein",
-      "Lithuania",
-      "Luxembourg",
-      "Macao",
-      "Macedonia",
-      "Madagascar",
-      "Malawi",
-      "Malaysia",
-      "Maldives",
-      "Mali",
-      "Malta",
-      "Marshall Islands",
-      "Martinique",
-      "Mauritania",
-      "Mauritius",
-      "Mayotte",
-      "McDonald Islands",
-      "Mexico",
-      "Micronesia",
-      "Miquelon",
-      "Moldova",
-      "Monaco",
-      "Mongolia",
-      "Montenegro",
-      "Montserrat",
-      "Morocco",
-      "Mozambique",
-      "Myanmar",
-      "Namibia",
-      "Nauru",
-      "Nepal",
-      "Netherlands",
-      "Netherlands Antilles",
-      "Nevis",
-      "New Caledonia",
-      "New Zealand",
-      "Nicaragua",
-      "Niger",
-      "Nigeria",
-      "Niue",
-      "Norfolk Island",
-      "Northern Mariana Islands",
-      "Norway",
-      "Oman",
-      "Pakistan",
-      "Palau",
-      "Palestinian Territory, Occupied",
-      "Panama",
-      "Papua New Guinea",
-      "Paraguay",
-      "Peru",
-      "Philippines",
-      "Pitcairn",
-      "Poland",
-      "Portugal",
-      "Principe",
-      "Puerto Rico",
-      "Qatar",
-      "Reunion",
-      "Romania",
-      "Russian Federation",
-      "Rwanda",
-      "Saint Barthelemy",
-      "Saint Helena",
-      "Saint Kitts",
-      "Saint Lucia",
-      "Saint Martin (French part)",
-      "Saint Pierre",
-      "Saint Vincent",
-      "Samoa",
-      "San Marino",
-      "Sao Tome",
-      "Saudi Arabia",
-      "Senegal",
-      "Serbia",
-      "Seychelles",
-      "Sierra Leone",
-      "Singapore",
-      "Slovakia",
-      "Slovenia",
-      "Solomon Islands",
-      "Somalia",
-      "South Africa",
-      "South Georgia",
-      "South Sandwich Islands",
-      "Spain",
-      "Sri Lanka",
-      "Sudan",
-      "Suriname",
-      "Svalbard",
-      "Swaziland",
-      "Sweden",
-      "Switzerland",
-      "Syrian Arab Republic",
-      "Taiwan",
-      "Tajikistan",
-      "Tanzania",
-      "Thailand",
-      "The Grenadines",
-      "Timor-Leste",
-      "Tobago",
-      "Togo",
-      "Tokelau",
-      "Tonga",
-      "Trinidad",
-      "Tunisia",
-      "Turkey",
-      "Turkmenistan",
-      "Turks Islands",
-      "Tuvalu",
-      "Uganda",
-      "Ukraine",
-      "United Arab Emirates",
-      "United Kingdom",
-      "United States",
-      "Uruguay",
-      "US Minor Outlying Islands",
-      "Uzbekistan",
-      "Vanuatu",
-      "Vatican City State",
-      "Venezuela",
-      "Vietnam",
-      "Virgin Islands (British)",
-      "Virgin Islands (US)",
-      "Wallis",
-      "Western Sahara",
-      "Yemen",
-      "Zambia",
-      "Zimbabwe",
-    ];
   }
 }
--- client/src/app/models/item.model.ts
+++ client/src/app/models/item.model.ts
@@ -2,6 +2,7 @@ export interface ItemModel {
   name: string,
   price: string,
   category: string,
-  date: Date,
+  date: string,
+  image: string,
   _id: string
 }
