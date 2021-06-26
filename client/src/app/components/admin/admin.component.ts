94b972af759a98453a4e87292798240b6b1f7257
Liorb <liorbalba@gmail.com>
2021-06-26 16:30:43 +0300

    lironi

--- client/src/app/components/admin/admin.component.ts
+++ client/src/app/components/admin/admin.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from "@angular/core";
+import {Component, Injectable, OnInit} from "@angular/core";
 import { SocketService } from "src/app/services/socket.service";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {ItemModel} from "../../models/item.model";
+import {environment} from "../../../environments/environment";
+
+@Injectable({ providedIn: "root" })
 @Component({
   selector: "app-admin",
   templateUrl: "./admin.component.html",
@@ -7,13 +12,21 @@ import { SocketService } from "src/app/services/socket.service";
 })
 export class AdminComponent implements OnInit {
   usersCount: Number;
-
-  constructor(private socketService: SocketService) {}
+  itemName: string;
+  private uri: string = environment.apiBaseUrl;
+  constructor(private socketService: SocketService, private http: HttpClient) {}
 
   ngOnInit(): void {
     // Connect to socket.io server
     this.socketService.listen("userConnected").subscribe((data: Number) => {
       this.usersCount = data;
+    });
+  }
+
+  searchItemExists() {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.get<string>(`${this.uri}/items/exists/${this.itemName}`, {
+      headers: headers,
     });
   }
 }
