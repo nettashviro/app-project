91220f00af32fd6fc404de5229580d8018fe398a
nettashviro <netta.zhviro@gmail.com>
2021-06-27 21:19:22 +0300

    changes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { SocketService } from "./services/socket.service";
 
 @Component({
   selector: "app-root",
@@ -8,7 +9,9 @@ import { Component, OnInit } from "@angular/core";
 export class AppComponent implements OnInit {
   title = "client";
 
-  constructor() {}
+  constructor(private socketService: SocketService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.socketService.emit("onlineUserCount", null);
+  }
 }
